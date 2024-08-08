7cb2b123cb0ad40f83728ada5d9b89e3b1a2b443
dor <admdor@automizedev.dk>
2024-08-08 12:56:48 +0200

    small changes to build module

--- src/Build.Module.ps1
+++ src/Build.Module.ps1
@@ -1,57 +1,69 @@
+Param(
+    [Parameter(Mandatory = $false)]
+    $Name = "Az.RuntimeEnvironment",
+
+    [Parameter(Mandatory = $false)]
+    $Path = $PSScriptRoot
+)
+
 $ErrorActionPreference = "Stop"
-try {
-    $Name = "Az.RuntimeEnvironment"
-    $Version = "1.0.3"
+
+try {    
     
-    $CurrentDirectory = $PSScriptRoot
-    $ModulePath = "$CurrentDirectory/$Name"
+    $ModulePath = "$Path/$Name"
     
-    #Changing Directory to the module root
-    Set-Location $CurrentDirectory
-    
-    #Getting Current Manifest Info
-    if (Test-Path "$ModulePath/$Name.psd1"){
-        $ManifestInfo = Test-ModuleManifest -Path "$ModulePath/$Name.psd1"
+    # Get Current Module Information
+    $Module = Find-Module $Name -Repository PSGallery -ErrorAction SilentlyContinue
+
+    # If module does not exist, then we will create a new module
+    if ($null -eq $Module) {
+        $NewVersion = 1.0.0
     }
+    else {
+        # Calculate new version
+        $CurrentVersion = [Version]$Module.Version
     
+        #Upping version
+        if ($CurrentVersion.Minor -eq 99) {
+            $NewVersion = New-Object System.Version(($CurrentVersion.Major + 1), 0, 0)
+        }
+        if ($CurrentVersion.Build -eq 9) {
+            $NewVersion = New-Object System.Version($CurrentVersion.Major, ($CurrentVersion.Minor + 1), 0)
+        }
+        else {
+            $NewVersion = New-Object System.Version($CurrentVersion.Major, $CurrentVersion.Minor, ($CurrentVersion.Build + 1))
+        }
+    }
+
     #Getting Public Functions to Export
     $PublicFunctions = Get-ChildItem -Path "$ModulePath/Public" -Recurse -Filter "*.ps1"
-
     
     #Creating Module Manifest
     $Params = @{
-        Author = "daoradmin"
-        CompanyName = "daoradmin"
-        Description = "Powershell Module for Azure Runtime Environment"
-        ModuleVersion = $Version
+        Author            = "daoradmin"
+        CompanyName       = "daoradmin"
+        Description       = "Powershell Module for Azure Runtime Environment"
+        ModuleVersion     = $NewVersion
         PowerShellVersion = "7.2"
-        Path = "$ModulePath/$Name.psd1"
+        Path              = "$ModulePath/$Name.psd1"
         FunctionsToExport = $PublicFunctions | Select-Object -ExpandProperty BaseName
-        ProjectUri = "https://github.com/daoradmin/$Name"
-        LicenseUri = "https://github.com/daoradmin/$Name/blob/master/LICENSE.txt"
-        Tags = @("PSMODULE", "$Name")
-        RootModule = "$Name.psm1"
+        ProjectUri        = "https://github.com/daoradmin/$Name"
+        LicenseUri        = "https://github.com/daoradmin/$Name/blob/master/LICENSE.txt"
+        Tags              = @("PSMODULE", "$Name")
+        RootModule        = "$Name.psm1"
     }
     
-    #Checking if the module version is the same as the current version
-    #If it is the same, then we will not update the module version
-    if ($Params.ModuleVersion -eq $ManifestInfo.Version){
-        Write-Warning "Module version is the same as the current version"
-        Write-Warning "Please update the module version!"
-        return
-    }
-    
-    #Creating Module Manifest
+    # Creating Module Manifest
     New-ModuleManifest @Params
     
-    #Creating OuraRing.psm1 file
+    # Creating module file
     New-Item -Path "$ModulePath/$Name.psm1" -ItemType File -Force | Out-Null
     
     $AllFunctions = @()
     $AllFunctions += (Get-ChildItem -Path "$ModulePath/Private" -Recurse -Filter "*.ps1")
     $AllFunctions += $PublicFunctions
     
-    foreach ($Function in $AllFunctions){
+    foreach ($Function in $AllFunctions) {
         $Content = Get-Content -Path $Function.ResolvedTarget
         #Adding content to OuraRing.psm1 file
         Add-Content -Path "$ModulePath/$Name.psm1" -Value $Content
